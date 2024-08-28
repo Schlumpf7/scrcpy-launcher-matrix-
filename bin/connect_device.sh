dd4385a29843426e3d0e664f81173e600cb18706
Schlumpf7 <77499857+Schlumpf7@users.noreply.github.com>
2024-08-28 10:54:40 +0200

    Update connect_device.sh

--- bin/connect_device.sh
+++ bin/connect_device.sh
@@ -9,7 +9,7 @@ else
 fi
 
 if [ "$PLATFORM" == "aarch64" ]; then
-  LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.kodi/addons/script.scrcpy-launcher/bin/lib ~/.kodi/addons/script.scrcpy-launcher/bin/adb_arm64 connect "$addr"
+  LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.kodi/addons/script.scrcpy-launcher/bin/lib_arm64 ~/.kodi/addons/script.scrcpy-launcher/bin/adb_arm64 connect "$addr"
 else
   LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.kodi/addons/script.scrcpy-launcher/bin/lib ~/.kodi/addons/script.scrcpy-launcher/bin/adb connect "$addr"
 fi
