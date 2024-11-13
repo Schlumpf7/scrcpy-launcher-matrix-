10bd2339acfa259bdaf40ef41f194c15f584647d
Schlumpf7 <77499857+Schlumpf7@users.noreply.github.com>
2024-11-13 19:09:10 +0100

    Update connect_device.sh

--- bin/connect_device.sh
+++ bin/connect_device.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
+
+# Set platform-dependent variables
 PLATFORM=$(uname -m)
-# Get IP address
-if [ -z "$1" ]; then
-    echo "Error, please specify IP address...exiting"
+ADDR="$1"
+
+# Verify the IP address is provided
+if [ -z "$ADDR" ]; then
+    echo "Error: Please specify IP address...exiting"
     exit 1
-else
-    addr="$1"
 fi
 
+# Define library paths based on architecture
 if [ "$PLATFORM" == "aarch64" ]; then
-  LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.kodi/addons/script.scrcpy-launcher/bin/lib_arm64 ~/.kodi/addons/script.scrcpy-launcher/bin/lib_arm64/adb connect "$addr"
+    LIB_PATH=~/.kodi/addons/script.scrcpy-launcher/bin/lib_arm64
 else
-  LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.kodi/addons/script.scrcpy-launcher/bin/lib ~/.kodi/addons/script.scrcpy-launcher/bin/lib/adb connect "$addr"
+    LIB_PATH=~/.kodi/addons/script.scrcpy-launcher/bin/lib
+fi
+
+# Connect to device using adb with correct environment variables
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LIB_PATH \
+"$LIB_PATH/adb" connect "$ADDR"
+
+# Check if adb connection was successful
+if [ $? -eq 0 ]; then
+    echo "Device connected successfully."
+else
+    echo "Failed to connect to device. Please verify IP and adb status."
+    exit 1
 fi
