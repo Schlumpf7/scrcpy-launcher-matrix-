528993a541ee2069a2ed9d041bf5db4cfdf5a98f
Schlumpf7 <77499857+Schlumpf7@users.noreply.github.com>
2024-11-13 18:59:02 +0100

    Update main.py
    Cleaner?

--- main.py
+++ main.py
@@ -12,50 +12,58 @@ arch = platform.machine()
 
 def adb_connect_to_device(addr):
     try:
-        result = subprocess.check_output("~/.kodi/addons/script.scrcpy-launcher/bin/connect_device.sh "+addr, shell=True)
-
-        if not "connected" in result:
-            xbmcgui.Dialog().ok("Error connecting to device", result)
+        result = subprocess.check_output(
+            f"~/.kodi/addons/script.scrcpy-launcher/bin/connect_device.sh {addr}", shell=True
+        )
+        if "connected" not in result.decode():
+            xbmcgui.Dialog().ok("Error connecting to device", result.decode())
             return False
-        else:
-            return True
-
+        return True
     except CalledProcessError as procError:
-        msg = procError.output
-        xbmcgui.Dialog().ok("Error connecting to device", msg)
+        xbmcgui.Dialog().ok("Error connecting to device", procError.output.decode())
         return False
 
+addon = xbmcaddon.Addon(id='script.scrcpy-launcher')
 
-addon = xbmcaddon.Addon(id='script.scrcpy-launcher')  
 while True:
-    opt = xbmcgui.Dialog().contextmenu(['Stream Device','Settings'])
+    opt = xbmcgui.Dialog().contextmenu(['Stream Device', 'Settings'])
+    
     if opt == 0:
         addr = addon.getSetting('ipaddress')
-        if b'adb_connect_to_device(addr)':
+        
+        if adb_connect_to_device(addr):
             while True:
-                # check if USB debugging was authorized
-                if arch == 'aarch64':
-                    launchCommand = "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.kodi/addons/script.scrcpy-launcher/bin/lib_arm64 ~/.kodi/addons/script.scrcpy-launcher/bin/lib_arm64/adb devices"
-                    result = subprocess.check_output(launchCommand, shell=True)
-                elif arch == 'armhf':
-                    launchCommand = "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.kodi/addons/script.scrcpy-launcher/bin/lib ~/.kodi/addons/script.scrcpy-launcher/bin/lib/adb devices"
-                    result = subprocess.check_output(launchCommand, shell=True)
-             
-                elif b"unauthorized" in result:
-                    xbmcgui.Dialog().ok("Unauthorized", "Please allow USB debugging connection in Android device")
-                else:
-                    break
+                try:
+                    # Check if USB debugging was authorized
+                    if arch == 'aarch64':
+                        launch_command = "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.kodi/addons/script.scrcpy-launcher/bin/lib_arm64 ~/.kodi/addons/script.scrcpy-launcher/bin/lib_arm64/adb devices"
+                    elif arch == 'armhf':
+                        launch_command = "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.kodi/addons/script.scrcpy-launcher/bin/lib ~/.kodi/addons/script.scrcpy-launcher/bin/lib/adb devices"
+                    else:
+                        xbmcgui.Dialog().ok("Unsupported Architecture", f"Architecture {arch} is not supported.")
+                        sys.exit()
+
+                    result = subprocess.check_output(launch_command, shell=True).decode()
                     
+                    if "unauthorized" in result:
+                        xbmcgui.Dialog().ok("Unauthorized", "Please allow USB debugging connection on the Android device")
+                    else:
+                        break
+                except CalledProcessError as procError:
+                    xbmcgui.Dialog().ok("Error", f"Failed to execute command: {procError}")
+                    break
+            
+            # Retrieve settings and construct launch command
             fps = addon.getSetting('fps')
             size = addon.getSetting('size')
             crop = addon.getSetting('crop')
             bitrate = addon.getSetting('bitrate')
-            
-            launchCommand = "systemd-run bash ~/.kodi/addons/script.scrcpy-launcher/bin/launch_scrcpy.sh"
-            args = ' "{}" "{}" "{}" "{}"'.format(fps,size,crop,bitrate)
-            os.system(launchCommand + args)
-        
+
+            launch_command = f"systemd-run bash ~/.kodi/addons/script.scrcpy-launcher/bin/launch_scrcpy.sh '{fps}' '{size}' '{crop}' '{bitrate}'"
+            os.system(launch_command)
+
         sys.exit()
+
     elif opt == 1:
         addon.openSettings()
     else:
