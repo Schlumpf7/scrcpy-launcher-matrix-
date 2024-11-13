45a98505f1d612233ab01b6d06e4382607e635b2
Schlumpf7 <77499857+Schlumpf7@users.noreply.github.com>
2024-11-13 18:29:30 +0100

    Update launch_scrcpy.sh
    changed paths - maybe it works now?

--- bin/launch_scrcpy.sh
+++ bin/launch_scrcpy.sh
@@ -34,12 +34,12 @@ fi
 
 systemctl stop kodi # Must close kodi for proper video display
 
-bin=~/.kodi/addons/script.scrcpy-launcher/bin
-
 if [ "$PLATFORM" == "aarch64" ]; then
-  SDL_VIDEO_EGL_DRIVER=libEGL.so SDL_VIDEO_GL_DRIVER=libGLESv2.so LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$bin/lib_arm64 PATH=$PATH:$bin SCRCPY_SERVER_PATH=$bin/lib_arm64/scrcpy-server $bin/lib_arm64/scrcpy --max-fps "$fps" -m "$size" --crop "$crop" --bit-rate "$bitrate" 
+  bin=~/.kodi/addons/script.scrcpy-launcher/bin/lib_arm64
+  SDL_VIDEO_EGL_DRIVER=libEGL.so SDL_VIDEO_GL_DRIVER=libGLESv2.so LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$bin PATH=$PATH:$bin SCRCPY_SERVER_PATH=$bin/scrcpy-server $bin/scrcpy --max-fps "$fps" -m "$size" --crop "$crop" --bit-rate "$bitrate" 
 else
-  SDL_VIDEO_EGL_DRIVER=libEGL.so SDL_VIDEO_GL_DRIVER=libGLESv2.so LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$bin/lib PATH=$PATH:$bin SCRCPY_SERVER_PATH=$bin/lib/scrcpy-server $bin/lib/scrcpy --max-fps "$fps" -m "$size" --crop "$crop" --bit-rate "$bitrate" 
+  bin=~/.kodi/addons/script.scrcpy-launcher/bin/lib
+  SDL_VIDEO_EGL_DRIVER=libEGL.so SDL_VIDEO_GL_DRIVER=libGLESv2.so LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$bin PATH=$PATH:$bin SCRCPY_SERVER_PATH=$bin/scrcpy-server $bin/scrcpy --max-fps "$fps" -m "$size" --crop "$crop" --bit-rate "$bitrate" 
 fi
 systemctl start kodi # Reopen kodi
 
